0c182061765ce1f16cc4bbe3af05c371c3f9458e
jamband <tmsongbooks215@gmail.com>
2013-10-29 12:45:38 +0900

    Fix style

--- protected/views/word/_listEdit.php
+++ protected/views/word/_listEdit.php
@@ -1,4 +1,5 @@
 <span class="word"><?php echo h($data->en); ?></span>
 <span class="word"><?php echo h($data->ja); ?></span>
-<?php echo l('update', array('update', 'id' => $data->id)); ?>
-<?php echo l('delete', '#', array('submit' => array('delete', 'id' => $data->id), 'confirm' => param('confirmDelete'), 'csrf' => true)); ?><br />
+<?php echo l('Update', array('update', 'id' => $data->id)); ?>
+<?php echo l('Delete', '#', array('submit' => array('delete', 'id' => $data->id), 'confirm' => param('confirmDelete'), 'csrf' => true)); ?>
+<br />
