affcb71d0285a67c63492941c648472718875199
jamband <tmsongbooks215@gmail.com>
2013-12-19 08:14:21 +0900

    Improve db test data and remove functional test one line

--- protected/tests/fixtures/word.php
+++ protected/tests/fixtures/word.php
@@ -1,36 +1,43 @@
 <?php
 return array(
 'Word_1' => array(
+    'id' => '1',
     'user_id' => '1',
     'en' => 'aaa',
     'ja' => 'えーえーえー',
 ),
 'Word_2' => array(
+    'id' => '2',
     'user_id' => '1',
     'en' => 'bbb',
     'ja' => 'びーびーびー',
 ),
 'Word_3' => array(
+    'id' => '3',
     'user_id' => '1',
     'en' => 'ccc',
     'ja' => 'しーしーしー',
 ),
 'Word_4' => array(
+    'id' => '4',
     'user_id' => '1',
     'en' => 'ddd',
     'ja' => 'でぃーでぃーでぃー',
 ),
 'Word_5' => array(
+    'id' => '5',
     'user_id' => '1',
     'en' => 'eee',
     'ja' => 'いーいーいー',
 ),
 'Word_6' => array(
+    'id' => '6',
     'user_id' => '2',
     'en' => 'abc',
     'ja' => 'えーびーしー',
 ),
 'Word_7' => array(
+    'id' => '7',
     'user_id' => '2',
     'en' => 'def',
     'ja' => 'でぃーいーえふ',
--- protected/tests/functional/WordTest.php
+++ protected/tests/functional/WordTest.php
@@ -41,7 +41,6 @@ class WordTest extends WebTestCase
         $this->type('name=Word[en]', '　　hello　');
         $this->type('name=Word[ja]', '　　こんにちは　');
         $this->clickAndWait("//input[@value='登録する']");
-        $this->assertLocation(TEST_BASE_URL.'word/8');
         $this->assertTextPresent('英単語の追加が完了いたしました。');
         $this->assertTextNotPresent('　　hello　');
         $this->assertTextNotPresent('　　こんにちは　');
