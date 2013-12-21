aaa960a6f000bc5d7e677c285074795103f79fd3
jamband <tmsongbooks215@gmail.com>
2013-12-21 18:05:47 +0900

    Move favicon.ico

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,7 +6,7 @@
     <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"><![endif]-->
     <!--[if lt IE 9]><script src="<?php echo bu(); ?>/js/html5shiv.js"></script><![endif]-->
     <link rel="stylesheet" type="text/css" href="<?php echo bu(); ?>/css/main.css">
-    <link rel="shortcut icon" type="image/x-icon" href="<?php echo bu(); ?>/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo bu(); ?>/images/ico-favicon.ico">
     <title><?php echo h($this->pageTitle); ?></title>
 </head>
 <body>
