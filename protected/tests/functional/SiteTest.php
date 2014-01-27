42a20a4d2dc70318d64cb39d2508fd72d37c8cad
jamband <tmsongbooks215@gmail.com>
2014-01-27 12:07:08 +0900

    Fix indent style

--- protected/tests/functional/SiteTest.php
+++ protected/tests/functional/SiteTest.php
@@ -11,11 +11,11 @@ class SiteTest extends WebTestCase
     public function testLoginLogout()
     {
         $this->open('');
-        
+
         if ($this->isTextPresent(WebTestCase::LOGOUT_BUTTON)) {
             $this->clickAndWait('link='.WebTestCase::LOGOUT_BUTTON);
         }
-            
+
         // test login
         $this->login('', '');
         $this->assertTextPresent('ログイン情報が正しくありません。');
@@ -25,10 +25,10 @@ class SiteTest extends WebTestCase
 
         $this->login('', WebTestCase::PASSWORD);
         $this->assertTextPresent('ログイン情報が正しくありません。');
-        
+
         $this->login();
         $this->assertTextPresent(WebTestCase::LOGOUT_BUTTON);
-        
+
         // test logout
         $this->clickAndWait('link='.WebTestCase::LOGOUT_BUTTON);
         $this->assertLocation(TEST_BASE_URL.'login');
