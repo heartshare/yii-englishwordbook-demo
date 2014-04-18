c26d01b9ad4b8ae964647bcb6a767e7efa311ef9
jamband <tmsongbooks215@gmail.com>
2014-04-18 22:47:53 +0900

    Improve word fixture data

--- protected/tests/fixtures/word.php
+++ protected/tests/fixtures/word.php
@@ -2,37 +2,37 @@
 return array(
 'Word_1' => array(
     'user_id' => '1',
-    'en' => 'aaa',
-    'ja' => 'えーえーえー',
+    'en' => 'beer',
+    'ja' => 'ビール',
 ),
 'Word_2' => array(
     'user_id' => '1',
-    'en' => 'bbb',
-    'ja' => 'びーびーびー',
+    'en' => 'cat',
+    'ja' => 'ねこ',
 ),
 'Word_3' => array(
     'user_id' => '1',
-    'en' => 'ccc',
-    'ja' => 'しーしーしー',
+    'en' => 'apple',
+    'ja' => 'りんご',
 ),
 'Word_4' => array(
     'user_id' => '1',
-    'en' => 'ddd',
-    'ja' => 'でぃーでぃーでぃー',
+    'en' => 'eat',
+    'ja' => '食べる',
 ),
 'Word_5' => array(
     'user_id' => '1',
-    'en' => 'eee',
-    'ja' => 'いーいーいー',
+    'en' => 'desk',
+    'ja' => 'つくえ',
 ),
 'Word_6' => array(
     'user_id' => '2',
-    'en' => 'abc',
-    'ja' => 'えーびーしー',
+    'en' => 'ant',
+    'ja' => 'アリ',
 ),
 'Word_7' => array(
     'user_id' => '2',
-    'en' => 'def',
-    'ja' => 'でぃーいーえふ',
+    'en' => 'deep',
+    'ja' => '深い',
 ),
 );
