02cf53a901963053b1d859f35f707966be2cb671
jamband <tmsongbooks215@gmail.com>
2014-04-21 18:59:38 +0900

    Change date function to DateTime class

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -35,7 +35,7 @@
     </div><!-- /#main -->
 
     <footer id="footer">
-        &copy; <?php echo date('Y'); ?> <?php echo app()->name; ?>
+        &copy; <?php echo date_create()->format('Y'); ?> <?php echo app()->name; ?>
     </footer><!-- /#footer -->
 
 </div><!-- /#container -->
