6f848ebd6b57d2b4d79b6bfe9b97b7195a4594c5
Tomoki Morita <tmsongbooks215@gmail.com>
2014-09-28 12:40:07 +0900

    Delete autoload.php line in protected/yii.php

--- protected/yiic.php
+++ protected/yiic.php
@@ -5,4 +5,3 @@ $yiic = dirname(__FILE__) . '/vendor/yiisoft/yii/framework/yiic.php';
 $config = dirname(__FILE__) . '/config/console.php';
 
 require_once($yiic);
-require(dirname(__FILE__) . '/protected/vendor/autoload.php');
