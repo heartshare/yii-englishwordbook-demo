ec4b17676aa577163f45dee643e14e5493aebcaf
Tomoki Morita <tmsongbooks215@gmail.com>
2014-10-24 08:49:56 +0900

    Change src attribute in html5shiv.js and respond.js

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo bu(); ?>/css/styles.css" media="all">
     <!--[if lt IE 9]>
-        <script src="<?php echo bu(); ?>/js/html5shiv.min.js"></script>
-        <script src="<?php echo bu(); ?>/js/respond.min.js"></script>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <title><?php echo h($this->pageTitle); ?></title>
 </head>
