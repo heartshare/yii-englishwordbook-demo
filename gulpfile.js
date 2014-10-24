25ece5838e206d46088dce4e2098a50ae73d1091
Tomoki Morita <tmsongbooks215@gmail.com>
2014-10-24 11:04:48 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,28 @@ var del         = require('del');
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 
-// task: js
-gulp.task('js', function() {
+// task: init
+gulp.task('init', function() {
   gulp.src('./bower_components/jquery-placeholder/jquery.placeholder.js')
     .pipe($.rename('jquery.placeholder.min.js'))
-    .pipe($.uglify())
+    .pipe($.uglify({preserveComments: 'some'}))
     .pipe(gulp.dest('js'));
 
-  gulp.src('./js/bootstrap.js')
-    .pipe($.rename('bootstrap.min.js'))
-    .pipe($.uglify())
+  gulp.src('./bower_components/bootstrap/dist/js/bootstrap.min.js')
     .pipe(gulp.dest('js'));
+
+  gulp.src('./bower_components/bootstrap/fonts/*')
+    .pipe(gulp.dest('fonts/bootstrap'));
+});
+
+// task: less
+gulp.task('less', function() {
+  gulp.src('./protected/less/styles.less')
+    .pipe($.less())
+    .pipe($.autoprefixer('last 2 version', 'ie 8'))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('./css'))
+    .pipe(reload({stream: true, once: true}));
 });
 
 // task: browserSync
@@ -31,15 +42,12 @@ gulp.task('browserSync', function() {
 // task: clean
 gulp.task('clean', del.bind(null, [
   'bower_components',
-  'node_modules',
-  'js/bootstrap',
-  'js/bootstrap.js',
-  'js/bootstrap-sprockets.js',
-  'protected/sass/.sass-cache'
+  'node_modules'
 ]));
 
 // task: default
 gulp.task('default', ['browserSync'], function() {
+  gulp.watch('./less/*.less', ['less']);
   gulp.watch('./protected/{views,widgets}/**/*.php', reload);
   gulp.watch('./**/css/*.css', reload);
 });
