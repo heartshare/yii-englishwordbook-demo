10021035f72b6987e2146ac779249b8795d65149
Tomoki Morita <tmsongbooks215@gmail.com>
2014-11-04 23:57:58 +0900

    Add link tag for favicon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo bu(); ?>/css/styles.css" media="all">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo bu(); ?>/images/ico-favicon.ico">
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
